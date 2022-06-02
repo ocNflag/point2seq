242cff03510e235b333f80e46757bdab8faf8ca6
Xue Yujing <18732034+ocNflag@users.noreply.github.com>
2022-06-02 12:37:39 +0800

    Update __init__.py

--- pcdet/models/dense_heads/__init__.py
+++ pcdet/models/dense_heads/__init__.py
@@ -6,15 +6,8 @@ from .point_head_simple import PointHeadSimple
 from .point_intra_part_head import PointIntraPartOffsetHead
 from .anchor_head_seg import AnchorHeadSeg
 from .center_head import CenterHead
-from .mm_head import MMHead
-from .e2e_head import E2EHead
-from .fusion_head import FusionHead
-from .attention_fusion_head import AttnFusionHead
-from .e2e_fusion_head import E2EFusionHead
 from .e2e_seqfuse_head import E2ESeqFusionHead
 from .e2e_seq_head import E2ESeqHead
-from .e2e_refine_head import E2ERefinementHead
-from .e2e_seq_token_head import E2ESeqTokenHead
 
 
 
@@ -29,13 +22,6 @@ __all__ = {
     'AnchorHeadMulti': AnchorHeadMulti,
     'AnchorHeadSeg': AnchorHeadSeg,
     'CenterHead': CenterHead,
-    'MMHead': MMHead,
-    'E2EHead': E2EHead,
-    'FusionHead': FusionHead,
-    'AttnFusionHead': AttnFusionHead,
-    'E2EFusionHead': E2EFusionHead,
     'E2ESeqFusionHead': E2ESeqFusionHead,
     'E2ESeqHead': E2ESeqHead,
-    'E2ESeqTokenHead': E2ESeqTokenHead,
-    'E2ERefinementHead': E2ERefinementHead
 }
